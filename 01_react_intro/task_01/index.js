bb5c02b35a431eb1712e805a3f71465f75bea124
apache0505 <jiayulutsk@ukr.net>
2022-11-25 13:15:45 +0200

    dw

---
+++ 01_react_intro/task_01/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+
+// put your code here
+
+const rootElement = document.querySelector('#root');
+
+const greetingElem = React.createElement(
+  'div',
+  { className: 'greeting' },
+  'Hello, React!'
+);
+
+ReactDOM.render(greetingElem, rootElement);
