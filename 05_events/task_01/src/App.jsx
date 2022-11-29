55ef2de511e8deb593d91928d2388748c7d3e3ef
apache0505 <jiayulutsk@ukr.net>
2022-11-29 13:28:06 +0200

    21

---
+++ 05_events/task_01/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoodButton from './GoodButton';
+
+const App = () => {
+  return (
+    <>
+      <GoodButton start={17} interval={1000} />
+    </>
+  );
+};
+
+export default App;
