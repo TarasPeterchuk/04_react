1715020a52e498fb36b0a742593e5a31eee5bb82
apache0505 <jiayulutsk@ukr.net>
2022-11-30 12:51:40 +0200

    dw

--- 06_conditional_rendering/task_hw_02/src/Auth.jsx
+++ 06_conditional_rendering/task_hw_02/src/Auth.jsx
@@ -7,7 +7,7 @@ class Auth extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: true,
+      isLoggedIn: false,
       spinnerVisible: false,
     };
   }
@@ -26,13 +26,13 @@ class Auth extends React.Component {
   handleLogin = () => {
     this.setSpinnerVisible();
     this.setState({
-      isLoggedIn: false,
+      isLoggedIn: true,
     });
   };
   handleLogout = () => {
     this.setSpinnerVisible();
     this.setState({
-      isLoggedIn: true,
+      isLoggedIn: false,
     });
   };
 
@@ -42,9 +42,9 @@ class Auth extends React.Component {
         {this.state.spinnerVisible ? (
           <Spinner size={50} />
         ) : this.state.isLoggedIn ? (
-          <Login onLogin={this.handleLogin} />
-        ) : (
           <Logout onLogout={this.handleLogout} />
+        ) : (
+          <Login onLogin={this.handleLogin} />
         )}
       </>
     );
