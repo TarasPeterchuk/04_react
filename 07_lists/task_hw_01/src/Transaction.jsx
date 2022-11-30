0e7a924d9d25194fb9f8f35c201abbefb33102b9
apache0505 <jiayulutsk@ukr.net>
2022-11-30 17:24:45 +0200

    das

---
+++ 07_lists/task_hw_01/src/Transaction.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import moment from 'moment';
+
+const Transaction = (transaction) => {
+  moment.locale('en-gb');
+  return (
+    <li className="transaction">
+      <span className="transaction__date">
+        {moment(transaction.time).format('D  MMM')}
+      </span>
+      <span className="transaction__time">
+        {' '}
+        {moment(transaction.time).format('LT')}
+      </span>
+      <span className="transaction__assets">
+        {transaction.from} → {transaction.to}
+      </span>
+      <span className="transaction__rate">{transaction.rate}</span>
+      <span className="transaction__amount">
+        {new Intl.NumberFormat('en-GB').format(transaction.amount)}
+      </span>
+    </li>
+  );
+};
+
+export default Transaction;
