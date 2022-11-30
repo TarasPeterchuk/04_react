10ad89b1ca1ee2f2f3e64ec1a3eac0b25e723efb
apache0505 <jiayulutsk@ukr.net>
2022-11-30 10:03:35 +0200

    dw

--- 06_conditional_rendering/task_01/src/Auth.jsx
+++ 06_conditional_rendering/task_01/src/Auth.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Greeting from './Greeting';
-import Login from './Login';
-import Logout from './Logout';
+// import Login from './Login';
+// import Logout from './Logout';
 
 class Auth extends React.Component {
   constructor(props) {
