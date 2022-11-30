c47e1191b15de4dff879f092c974d8062703f8a5
apache0505 <jiayulutsk@ukr.net>
2022-11-30 10:02:18 +0200

    das

---
+++ 06_conditional_rendering/task_01/src/Login.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Login = (props) => {
+  return <button onClick={this.handleLogout}>Logout</button>;
+};
+
+export default Login;
---
+++ 06_conditional_rendering/task_01/src/Logout.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Login = (props) => {
+  return <button onClick={this.handleLogin}>Login</button>;
+};
+
+export default Login;
