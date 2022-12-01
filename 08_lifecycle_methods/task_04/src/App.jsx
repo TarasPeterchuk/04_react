6765c27225a84c40ac89bba790dd2f7e0c7fb8c8
apache0505 <jiayulutsk@ukr.net>
2022-12-01 12:55:58 +0200

    dsa

---
+++ 08_lifecycle_methods/task_04/src/App.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import User from './User';
+
+class App extends Component {
+  render() {
+    return <User userId="facebook" />;
+  }
+}
+export default App;
