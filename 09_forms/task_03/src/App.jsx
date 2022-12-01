7180b3713b5bddc972471d618b95fb55b288bf64
apache0505 <jiayulutsk@ukr.net>
2022-12-01 18:29:17 +0200

    das

---
+++ 09_forms/task_03/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserForm from './UserForm';
+
+const App = () => {
+  const createUser = (obj) => {
+    console.log(obj);
+  };
+  return (
+    <>
+      <UserForm onSubmit={createUser} />
+    </>
+  );
+};
+
+export default App;
