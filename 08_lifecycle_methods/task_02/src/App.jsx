215d1998780a3e540a8e2503feb03640af6a30a5
apache0505 <jiayulutsk@ukr.net>
2022-12-01 11:51:22 +0200

    dsa

---
+++ 08_lifecycle_methods/task_02/src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true,
+    };
+    // this.toggle = this.toggle.bind(this);
+  }
+  toggle = () => {
+    // toggle() {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <button onClick={this.toggle}>Toggle</button>
+        {this.state.visible && (
+          <>
+            <Clock location="London" offset={0} />
+            <Clock location="Kyiv" offset={2} />
+            <Clock location="New York" offset={-5} />
+          </>
+        )}
+      </>
+    );
+  }
+}
+export default App;
