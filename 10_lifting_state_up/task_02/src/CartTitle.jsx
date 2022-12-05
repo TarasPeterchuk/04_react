15808e89be773d8846517dc582b5ec26bde0aa2b
apache0505 <jiayulutsk@ukr.net>
2022-12-05 11:16:03 +0200

    dawdwadas

---
+++ 10_lifting_state_up/task_02/src/CartTitle.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const CartTitle = ({ userName, count }) => {
+  return (
+    <div className="cart-title">{`${userName}, you added ${count} items`}</div>
+  );
+};
+
+export default CartTitle;
