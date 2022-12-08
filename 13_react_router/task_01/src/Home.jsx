28a9df3c899f054cfaf2d03e7e84c79239547500
apache0505 <jiayulutsk@ukr.net>
2022-12-08 12:10:42 +0200

    dqw

--- 13_react_router/task_01/src/Home.jsx
+++ 13_react_router/task_01/src/Home.jsx
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 const Home = () => {
   return (
-    <>
-      <div className="page__content">
-        <h1>🏠</h1>
-        <Link to="/products">To products</Link>
-      </div>
-    </>
+    <div className="page__content">
+      <h1>🏠</h1>
+      <Link to="/products">To products</Link>
+    </div>
   );
 };
 
