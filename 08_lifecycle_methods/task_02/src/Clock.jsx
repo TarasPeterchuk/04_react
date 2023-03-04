4f7362756775471d5e789f28bdf67e2bde4f26d0
apache0505 <jiayulutsk@ukr.net>
2023-03-04 12:24:46 +0200

    xs

--- 08_lifecycle_methods/task_02/src/Clock.jsx
+++ 08_lifecycle_methods/task_02/src/Clock.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
-const getTimeWithOffset = (offset) => {
+const getTimeWithOffset = offset => {
   const currentTime = new Date();
   const utcOffset = currentTime.getTimezoneOffset() / 60;
-  return new Date(
-    currentTime.setHours(currentTime.getHours() + offset + utcOffset)
-  );
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
 };
-const formatDate = (date) => moment(date).format('h:mm:ss A');
+const formatDate = date => moment(date).format('h:mm:ss A');
 
 class Clock extends Component {
   constructor(props) {
